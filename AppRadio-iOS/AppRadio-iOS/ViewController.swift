77a246f9418eb20009184fe13387e2be6b92c3c8
luibasantes <luibasantes@hotmail.com>
2018-09-12 17:19:57 -0500

    Crear el proyecto base para iOS [[#159672597]]

---
+++ AppRadio-iOS/AppRadio-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppRadio-iOS
+//
+//  Created by Luigi Basantes on 9/12/18.
+//  Copyright © 2018 INNOVASYSTEM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
